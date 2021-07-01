6466cc6da176b36d5f886ae98fe1c8e2ac6d1a00
txiong000 <tkx422@gmail.com>
2021-06-30 20:57:37 -0500

    add font-awesome, @media for mobile, hamburger icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign_up', component: SignupFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
