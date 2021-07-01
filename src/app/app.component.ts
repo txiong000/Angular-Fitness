df9dfed94784089e818fbaa38c2e32df91d1a7b1
txiong000 <tkx422@gmail.com>
2021-06-30 23:35:35 -0500

    create signup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 export class AppComponent {
   title = 'Fitness-Tracker-Angular';
   faBars = faBars;
+
+  switchForm:boolean = false;
+  signUpLogin() {
+    if(!this.switchForm)
+    {
+      this.switchForm = true;
+    } else {
+      this.switchForm = false;
+    }
+    console.log(this.switchForm)
+  }
 }
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent implements OnInit {
+  
+  switchForm:boolean = false;
+  signOrLog?:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signUpLogin() {
+    if(!this.switchForm)
+    {
+      this.switchForm = true;
+    } else {
+      this.switchForm = false;
+    }
+    console.log(this.switchForm)
   }
 
 }
